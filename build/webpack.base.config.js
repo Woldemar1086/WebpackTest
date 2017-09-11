9671cf3f49b7401a430c65ae9961d6c863e3aaac
Woldemar1086 <khodyrev.v.a@gmail.com>
2017-09-11 14:39:25 +0200

    added static loaders

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -15,5 +15,41 @@ module.exports = {
 					title: 'WebpackTest',
 					filename: 'index.html'
 				})
-	]
+	],
+	module:{
+		rules:[
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				include: [path.join(__dirname, '..', 'src')],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+				loader: 'url-loader',
+				options:{
+					limit: 10000,
+					name:path.join('static', 'img/[name].[hash:?].[ext]')
+				}
+			},
+			{
+				test:/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+				loader:'url-loader',
+				options:{
+					limit: 10000,
+					name: path.join('static','media/[name].[hash:?].[ext]')
+				}
+			},{
+				test:/\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				loader: 'url-loader',
+				options:{
+					limit:10000,
+					name: path.join('static', 'fonts/[name].[hash:?].[ext]')
+				}
+			}
+		]
+	}
 }
